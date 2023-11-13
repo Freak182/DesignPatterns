7104da57ff0f05492eeebd75ee926bf22a2963ae
TOUTANT, Danny (MTL) <Danny.Toutant@bdc.ca>
2023-11-13 05:01:31 -0500

    MappingProfile

--- Web.App/Database/Mapping/Profile/ReservationMappingProfile.cs
+++ Web.App/Database/Mapping/Profile/ReservationMappingProfile.cs
@@ -6,7 +6,12 @@ namespace Web.App.Database.Mapping.Profile
 {
     public class ReservationMappingProfile : AutoMapper.Profile
     {
-        CreateMap<Reservation, GetReservationResponse>()
-
+        public ReservationMappingProfile()
+        {
+            CreateMap<Reservation, GetReservationResponse>()
+                .ForMember(dest => dest.ReservationId, opt => opt.MapFrom(source => source.Id))
+                .ForMember(dest => dest.State, opt => opt.MapFrom(source => source.State))
+                .ForMember(dest => dest.Plane, opt => opt.MapFrom(source => source.Plane));
+        }
     }
 }
--- Web.App/Entity/Response/GetReservationResponse.cs
+++ Web.App/Entity/Response/GetReservationResponse.cs
@@ -1,10 +1,12 @@
-﻿namespace Web.App.Entity.Response
+﻿using System.Numerics;
+
+namespace Web.App.Entity.Response
 {
     public class GetReservationResponse
     {
         public int ReservationId { get; set; }
 
-        public int Plane { get; set; }
+        public Plane? Plane { get; set; }
 
         public string? State { get; set; }
     }
