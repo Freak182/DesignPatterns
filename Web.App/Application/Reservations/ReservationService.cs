f0af5386148c127e46f3b6f3a56770ee4dc2dcc9
TOUTANT, Danny (MTL) <Danny.Toutant@bdc.ca>
2023-11-13 03:34:05 -0500

    EntityWithId

--- Web.App/Application/Reservations/ReservationService.cs
+++ Web.App/Application/Reservations/ReservationService.cs
@@ -4,23 +4,11 @@ using Web.App.Database.DbSet;
 
 namespace Web.App.Application.Reservations
 {
-    public class ReservationService : IReservationService
+    public class ReservationService : EntityWithIdService<Reservation>, IReservationService
     {
-        protected readonly IReservationRepository _reservationRepository;
-
         public ReservationService(IReservationRepository reservationRepository)
-        {
-            _reservationRepository = reservationRepository;
-        }
-
-        /// <summary>
-        /// Get entity by id
-        /// </summary>
-        /// <param name="id">Entity id</param>
-        /// <returns></returns>
-        public Reservation GetById(int id)
-        {
-            return _reservationRepository.GetById(id);
+            : base(reservationRepository) 
+        { 
         }
     }
 }
--- Web.App/Application/Services/EntityWIthIdService.cs
+++ Web.App/Application/Services/EntityWIthIdService.cs
@@ -3,15 +3,16 @@ using Web.App.Database;
 
 namespace Web.App.Application.Common
 {
-    public class EntityWIthIdService<TEntity> : IEntityWithIdService<TEntity>
+    public class EntityWithIdService<TEntity> : IEntityWithIdService<TEntity>
         where TEntity : EntityWithIdBase
     {
         protected readonly IEntityWithIdRepository<TEntity> _entityRepository;
 
-        public EntityWIthIdService(IEntityWithIdRepository<TEntity> entityRepository)
+        public EntityWithIdService(IEntityWithIdRepository<TEntity> entityRepository)
         {
             _entityRepository = entityRepository;
         }
+
         /// <summary>
         /// Get entity by id
         /// </summary>
